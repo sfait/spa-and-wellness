4c3b1a41d2a7bb528ca3b9cd2de4570f49583eb0
Sylwia Fait <sylwia.fait@gmail.com>
2018-11-07 18:16:40 +0100

    Updated js for read-more-less-btn

--- js/app.js
+++ js/app.js
@@ -12,6 +12,25 @@ const init = () => {
         this.style.display = "none";
     });
 
+    // read more less
+    const readMoreLessBtn = document.querySelectorAll(".read-more-less-btn");
+
+    function showHideText() {
+        const siblingText = this.previousElementSibling;
+        
+       if (siblingText.style.display === "none" || siblingText.style.display === "") {
+            siblingText.style.display = "block";
+            this.textContent = "Czytaj mniej";
+        } else {
+            siblingText.style.display = "none";
+            this.textContent = "Czytaj więcej";
+        } 
+    }
+
+    for (let i = 0; i < readMoreLessBtn.length; i++) {
+        readMoreLessBtn[i].addEventListener("click", showHideText);
+    }
+
 }
 
 document.addEventListener("DOMContentLoaded", init);
