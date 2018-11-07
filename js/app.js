b52a4325790dc7b19d52fb6227f32a3db8073d18
Sylwia Fait <sylwia.fait@gmail.com>
2018-11-07 19:19:14 +0100

    Added js for newsletter

--- js/app.js
+++ js/app.js
@@ -16,8 +16,9 @@ const init = () => {
     const readMoreLessBtn = document.querySelectorAll(".read-more-less-btn");
 
     function showHideText() {
+        // find previous sibling
         const siblingText = this.previousElementSibling;
-        
+
        if (siblingText.style.display === "none" || siblingText.style.display === "") {
             siblingText.style.display = "block";
             this.textContent = "Czytaj mniej";
@@ -27,9 +28,28 @@ const init = () => {
         } 
     }
 
+    // add action for buttons
     for (let i = 0; i < readMoreLessBtn.length; i++) {
         readMoreLessBtn[i].addEventListener("click", showHideText);
     }
+
+    // newsletter
+    const input = document.querySelector(".newsletter-form input");
+    const newsletterBtn = document.querySelector(".newsletter-form button");
+    const formInfo = document.querySelector(".form-info");
+    
+    newsletterBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        formInfo.style.display = "block";
+
+        if (input.value !== "") {
+            formInfo.style.color = "#fff";
+            formInfo.textContent = "Zapisano do newslettera";
+        } else {
+            formInfo.style.color = "#444";
+            formInfo.textContent = "Pole nie może być puste, podaj adres e-mail";
+        }
+    })
 
 }
 
